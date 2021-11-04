7a869b97326150c6866551e745fdbd30e28573eb
deadManAlive <alfarrakhan@gmail.com>
2021-11-04 14:49:41 +0700

    use fmt to i/o and string formatting

--- include/structure.hpp
+++ include/structure.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <array>
+#include <fmt/core.h>
+#include <string>
+
 namespace riff{
     typedef struct RIFFHEADER{
         char chunkID[4]; //"RIFF"
@@ -8,7 +12,6 @@ namespace riff{
     } rhead;
 
     typedef struct WAVHEADER{
-        //WAV header (if RIFFHEADER.format == "WAVE")
         char subchunk1ID[4]; //"fmt "
         uint32_t subchunk1Size;
         uint16_t audioFormat; //audio storage format (PCM,...)
@@ -19,9 +22,88 @@ namespace riff{
         uint16_t bitDepths;
     } whead;
 
-    typedef struct WAVDATA{
+    template<typename T, uint32_t SIZE>
+    struct wdata{
         char subchunk2ID[4]; //"data"
         uint32_t subchunk2Size;
-        void *data; //here we go again...
-    } wdata;
+        std::array<T, SIZE> data_container;
+    };    
+}
+
+//RAII?
+namespace structure{
+    class rhead{
+        private:
+            char chunkID[4];
+            uint32_t chunkSize;
+            char format[4];
+        public:
+            std::string getChunkID(){
+                return fmt::format("{}{}{}{}", chunkID[0], chunkID[1], chunkID[2], chunkID[3]);
+            }
+            uint32_t getChunkSize(){
+                return chunkSize;
+            }
+            std::string getFormat(){
+                return fmt::format("{}{}{}{}", format[0], format[1], format[2], format[3]);
+            }
+    };
+
+    class whead{
+        private:
+            char subchunk1ID[4]; //"fmt "
+            uint32_t subchunk1Size;
+            uint16_t audioFormat; //audio storage format (PCM,...)
+            uint16_t numChannels;
+            uint32_t sampleRate;
+            uint32_t byteRate;
+            uint16_t blockAlign;
+            uint16_t bitDepth;
+        public:
+            std::string getSubChunk1ID(){
+                return fmt::format("{}{}{}{}", subchunk1ID[0], subchunk1ID[1], subchunk1ID[2], subchunk1ID[3]);
+            }
+            uint32_t getSubChunk1Size(){
+                return subchunk1Size;
+            }
+            uint16_t getAudioFormat(){
+                return audioFormat;
+            }
+            uint16_t getNumChannels(){
+                return numChannels;
+            }
+            uint32_t getSampleRate(){
+                return sampleRate;
+            }
+            uint32_t getByteRate(){
+                return byteRate;
+            }
+            uint16_t getBlockAlign(){
+                return blockAlign;
+            }
+            uint16_t getBitDepth(){
+                return bitDepth;
+            }
+    };
+
+    template<typename T, size_t SIZE>
+    class wdata{
+        private:
+            char subchunk2ID[4];
+            uint32_t subchunk2Size;
+            std::array<T, SIZE> data_container;
+        public:
+            std::string getSubChunk2ID(){
+                return fmt::format("{}{}{}{}", subchunk2ID[0], subchunk2ID[1], subchunk2ID[2], subchunk2ID[3]);
+            }
+            uint32_t getSubChunk2Size(){
+                return subchunk2Size;
+            }
+            T getDataAt(size_t pos){
+                return data_container.at(pos);
+            }
+            std::array<T, SIZE> getDataContainer(){
+                return data_container;
+            }
+    };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <string>
-#include <fmt/core.h>
-
 #include "structure.hpp"
 // #include "util.hpp"
 
@@ -19,29 +15,40 @@ int main(int argc, char **argv){
     fmt::print(fmt::format("Opening {}...\n", argv[1]));
 
     riff::rhead sample_riff_header_buffer;
-    // whead sample_wav_header_buffer;
+    riff::whead sample_wav_header_buffer;
     // wdata sample_wav_data_buffer;
 
     fread(&sample_riff_header_buffer, 12, 1, file_handle);
     
     //==RIFF HEADER==//
     //RIFF header display
-    fmt::print(fmt::format("RIFFHEADER:\t\t{}{}{}{}\n", sample_riff_header_buffer.chunkID[0], sample_riff_header_buffer.chunkID[1], sample_riff_header_buffer.chunkID[2], sample_riff_header_buffer.chunkID[3]));
+    fmt::print(fmt::format("RIFFHEADER:\t\t{}{}{}{}\n", sample_riff_header_buffer.chunkID[0],
+                                                        sample_riff_header_buffer.chunkID[1],
+                                                        sample_riff_header_buffer.chunkID[2],
+                                                        sample_riff_header_buffer.chunkID[3]));
 
     //RIFF size display
     fmt::print(fmt::format("TOTALSIZE:\t\t{} + 8 bytes\n", sample_riff_header_buffer.chunkSize));
 
     //RIFF format display
-    std::string srform = fmt::format("{}{}{}{}", sample_riff_header_buffer.format[0], sample_riff_header_buffer.format[1], sample_riff_header_buffer.format[2], sample_riff_header_buffer.format[3]);
-    fmt::print(fmt::format("RIFFFORMAT:\t\t{}\n", srform));
+    std::string srform = fmt::format("{}{}{}{}", sample_riff_header_buffer.format[0],
+                                                 sample_riff_header_buffer.format[1],
+                                                 sample_riff_header_buffer.format[2],
+                                                 sample_riff_header_buffer.format[3]);
+    fmt::print(fmt::format("RIFFFORMAT:\t\t\"{}\"\n", srform));
 
-    //WAV test
-    if(srform.compare("WAVE") != 0){
-        printf("This file is not supported!");
-        fmt::print("This file is not supported!");
-        fclose(file_handle);
-        return -3;
-    }
+    //==WAVE HEADER==/
+    fread(&sample_wav_header_buffer, 24, 1, file_handle);
+
+    fmt::print(fmt::format("FMT:\t\t\t\"{}{}{}{}\"\n", sample_wav_header_buffer.subchunk1ID[0],
+                                       sample_wav_header_buffer.subchunk1ID[1],
+                                       sample_wav_header_buffer.subchunk1ID[2],
+                                       sample_wav_header_buffer.subchunk1ID[3]));
+
+    fmt::print(fmt::format("Audio format:\t\t{}\n", sample_wav_header_buffer.audioFormat==1?"PCM":"Not a PCM"));
+    fmt::print(fmt::format("Channel numbers:\t{}\n", sample_wav_header_buffer.numChannels));
+    fmt::print(fmt::format("Sample rate:\t\t{}\n", sample_wav_header_buffer.sampleRate));
+    fmt::print(fmt::format("Bit depth:\t\t{}\n", sample_wav_header_buffer.bitDepths));
 
     fclose(file_handle);
     return 0;
