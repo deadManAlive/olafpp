0330d9b2d2b43f7786968933882fc9aaef657564
deadManAlive <alfarrakhan@gmail.com>
2022-03-08 20:33:15 +0700

    remove custom wav reading use libsndfile instead

---
+++ README.md
@@ -0,0 +1,12 @@
+# OLaF++
+The **o**ffline (not-necessarily) **l**ine**a**r **f**ilter using primitive convolution algorithm (no transform).
+
+The linear properties of the filter would be achieved by convolving input signal with symmetrical impulse array with length at least twice of the input signal.
+
+Libraries used in this project.:
+
+* `libsndfile`
+* `fmt`
+
+## Author
+This project is developed by deadManAlive a.k.a Rafky Alfarrakhan [alfarrakhan@gmail.com](alfarrakhan@gmail.com).
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <string>
 #include <fmt/core.h>
 #include <sndfile.hh>
+#include <chrono>
 
-#include <structure.hpp>
+// #include <structure.hpp>
+#include <convolve.h>
 
 int main(int argc, char **argv){
     if(argc < 2){
@@ -12,13 +14,28 @@ int main(int argc, char **argv){
 
     fmt::print(fmt::format("{} \n", argv[1]));
 
-    SndfileHandle file(argv[1]);
-
-    // return 0;
+    SndfileHandle file = SndfileHandle(argv[1]);
 
     fmt::print(fmt::format("Opened {}.\n", argv[1]));
     fmt::print(fmt::format("    Sample rate : {}\n", file.samplerate()));
     fmt::print(fmt::format("    Channels    : {}\n", file.channels()));
+    fmt::print(fmt::format("    Format      : {}\n", file.format()));
+
+    std::vector<double> samples(file.frames());
+    file.read(&samples[0], int(file.frames()));
+
+    fmt::print(fmt::format("    Samples     : {}\n", samples.size()));
+
+    std::vector<double> impulse{1, 0.5, 0.25, 0.125};
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::vector<double> res = convolve(samples, impulse); //res to output
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+    fmt::print(fmt::format("Finished convolving {} by {} samples in {} ms.\n", samples.size(), impulse.size(), duration.count()));
 
     return 0;
 }
