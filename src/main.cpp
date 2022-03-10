d2c8a0f17b58db53fae2749929c3a456c164c926
deadManAlive <alfarrakhan@gmail.com>
2022-03-10 10:27:23 +0700

    file cheking and code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,21 @@
 #include <fmt/core.h>
 #include <sndfile.hh>
 #include <chrono>
+#include <filesystem>
 
-// #include <structure.hpp>
 #include <convolve.h>
 
 int main(int argc, char **argv){
     if(argc < 2){
         fmt::print("No file provided!\n");
-        return -1;
+        return 2;
+    }
+    else if(!std::filesystem::exists(argv[1])){
+        fmt::print(fmt::format("File \"{}\" not found!\n", argv[1]));
+        return 2;
     }
 
-    fmt::print(fmt::format("{} \n", argv[1]));
+    //================OPENING FILE================//
 
     SndfileHandle file = SndfileHandle(argv[1]);
 
@@ -26,14 +30,18 @@ int main(int argc, char **argv){
 
     fmt::print(fmt::format("    Samples     : {}\n", samples.size()));
 
+    //================CONVOLUTION================
+
     std::vector<double> impulse{1, 0.5, 0.25, 0.125};
 
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now(); //clock in
 
     std::vector<double> res = convolve(samples, impulse); //res to output
 
-    auto stop = std::chrono::high_resolution_clock::now();
+    auto stop = std::chrono::high_resolution_clock::now(); //clock out
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+    //================POST PROCESS================
 
     fmt::print(fmt::format("Finished convolving {} by {} samples in {} ms.\n", samples.size(), impulse.size(), duration.count()));
 
